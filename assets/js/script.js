cbb3c9fb94f76556e16c9329eec04563b9ddc22f
Chelso86 <chelseaholmstead@gmail.com>
2021-11-18 17:51:05 -0700

    additions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+console.dir(window.document);
+
+// present home screen
+
+
+
+var buttonEl = document.querySelector("#start-quiz"); 
+var mainBodyEl = document.querySelector("#body"); 
+
+buttonEl.addEventListener("click", start-quiz);
+
+var presentquestion = function() { 
+  var qDiv = document.createElement("div");
+  qDiv.innerHTML = "<h1> QUESTION </h2>";
+  qDiv.innerHTML = "<div> </div>"; 
+  qDiv.className = "qDiv"; 
+  mainBodyEl.appendChild("#body"); 
+  }; 
+
+
+
+
+
+
+// start timer & begin quiz
+
+// present with question
+
+// subtract time
+
+// save initials & end
